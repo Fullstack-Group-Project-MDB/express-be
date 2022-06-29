55ca15e2c7edd5d4fe0bd7f2eec2f80397c601f1
m-ghiringhelli <mghiring@gmail.com>
2022-06-29 11:49:13 -0700

    wrote test for GET on /:id route for listings

--- __tests__/listing.test.js
+++ __tests__/listing.test.js
@@ -2,12 +2,12 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
-const { post } = require('../lib/app');
 
 describe('listing routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
+
   it('GET request to /listings should return a list of listings', async () => {
     const res = await request(app).get('/api/v1/listings');
 
@@ -17,6 +17,18 @@ describe('listing routes', () => {
     expect(lostDog.content).toBe('Help I lost my dog');
     expect(res.body.length > 1).toBe(true);
   });
+
+  it('GET request to /listings/:id should return detail', async () => {
+    const res = await request(app).get('/api/v1/listings/1');
+
+    expect(res.status).toBe(200);
+    expect(res.body).toEqual({
+      id: '1',
+      title: 'Lost dog',
+      content: 'Help I lost my dog'
+    });
+  });
+
   afterAll(() => {
     pool.end();
   });
