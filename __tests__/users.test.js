fc9a81eb80cdf362125ac08339fe2df5192fc854
Denver McCarthy <dmichaelspdx@gmail.com>
2022-06-29 17:55:58 -0700

    added session route

--- __tests__/users.test.js
+++ __tests__/users.test.js
@@ -2,9 +2,27 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const UserService = require('../lib/services/UserService');
 const mockUser = {
   email: 'mock@dummy.com',
   password: 'password',
+};
+const { email } = mockUser;
+
+const registerAndLogin = async (userProps = {}) => {
+  const password = userProps.password ?? mockUser.password;
+
+  // Create an "agent" that gives us the ability
+  // to store cookies between requests in a test
+  const agent = request.agent(app);
+
+  // Create a user to sign in with
+  const user = await UserService.create({ ...mockUser, ...userProps });
+
+  // ...then sign in
+  const { email } = user;
+  await agent.post('/api/v1/users/sessions').send({ email, password });
+  return [agent, user];
 };
 
 describe('listing routes', () => {
@@ -13,7 +31,6 @@ describe('listing routes', () => {
   });
 
   it('POST to /users should create a new user', async () => {
-    const { email } = mockUser;
     const res = await request(app).post('/api/v1/users').send(mockUser);
     expect(res.status).toBe(200);
     expect(res.body).toEqual({ id: expect.any(String), email });
